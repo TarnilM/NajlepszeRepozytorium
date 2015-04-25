3c274e20bf1e03c81bcd3dc96eea2dff29f10887
unknown <student@komp242.nmkzllab>
2015-04-25 14:32:42 +0200

    pierwszyCommit

---
+++ ConsoleApplication2/Kubelek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2 {
+    class Kubelek 
+    {
+        int IleKostek;
+        int IleScian;
+
+        Random rand = new Random();
+
+        public Kubelek(int ileKostek, int ileScian) 
+        {
+            this.IleKostek = ileKostek;
+            this.IleScian = ileScian;
+        }
+        public List<int> rzut() 
+        {
+            List<int> lista = new List<int>();
+            for (int i = 1; i < this.IleKostek; i++) 
+            {
+                lista.Add(rand.Next(0, IleScian));
+            }
+           return lista;
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Kubelek kubel = new Kubelek(5, 3);
+            List<int> wyniki = kubel.rzut();
+        }
+    }
+}
